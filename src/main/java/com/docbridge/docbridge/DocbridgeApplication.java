23df69958e9f6cf97c18c94255343063cf036e35
leetoneee <letoan7442@gmail.com>
2026-06-12 11:13:49 +0700

    Initial commit

---
+++ src/main/java/com/docbridge/docbridge/DocbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.docbridge.docbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocbridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docbridge/docbridge/DocbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docbridge.docbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

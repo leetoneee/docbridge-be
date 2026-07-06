6ca5575542808403c4c4744ba0f6e12c124dd610
leetoneee <letoan7442@gmail.com>
2026-07-06 18:43:27 +0700

    fix(CI): fix Test ElasticSearch Config

--- src/test/java/com/docbridge/docbridge/DocbridgeApplicationTests.java
+++ src/test/java/com/docbridge/docbridge/DocbridgeApplicationTests.java
@@ -2,12 +2,17 @@ package com.docbridge.docbridge;
 
 import com.docbridge.docbridge.config.TestConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.elasticsearch.enabled=false",
+		"spring.data.elasticsearch.repositories.enabled=false"
+})
 @Import(TestConfig.class)
+@AutoConfigureMockMvc
 class DocbridgeApplicationTests {
 
 	@Test
--- src/test/java/com/docbridge/docbridge/config/TestConfig.java
+++ src/test/java/com/docbridge/docbridge/config/TestConfig.java
@@ -1,7 +1,16 @@
 package com.docbridge.docbridge.config;
 
+import com.docbridge.docbridge.module.log.audit.AuditLogRepository;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import static org.mockito.Mockito.mock;
 
 @TestConfiguration
 public class TestConfig {
+    @Bean
+    @Primary
+    public AuditLogRepository auditLogRepository() {
+        return mock(AuditLogRepository.class);
+    }
 }

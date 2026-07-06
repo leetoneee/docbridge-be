4e5bee8b2daf37c2e85db75b1b67a00dee01861f
leetoneee <letoan7442@gmail.com>
2026-07-06 18:13:43 +0700

    setup CI

--- src/test/java/com/docbridge/docbridge/DocbridgeApplicationTests.java
+++ src/test/java/com/docbridge/docbridge/DocbridgeApplicationTests.java
@@ -2,8 +2,10 @@ package com.docbridge.docbridge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DocbridgeApplicationTests {
 
 	@Test

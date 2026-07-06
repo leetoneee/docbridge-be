5ea1fa36a507d1b1c375cbbae6d8e6205c8a14ed
leetoneee <letoan7442@gmail.com>
2026-07-06 18:29:43 +0700

    fix(CI): fix Test ElasticSearch Config

--- src/main/java/com/docbridge/docbridge/shared/config/ElasticsearchConfig.java
+++ src/main/java/com/docbridge/docbridge/shared/config/ElasticsearchConfig.java
@@ -6,10 +6,20 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.client.RestClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@EnableElasticsearchRepositories(
+        basePackages = "com.docbridge.docbridge.module.log.audit"
+)
+@ConditionalOnProperty(
+        name = "spring.data.elasticsearch.repositories.enabled",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class ElasticsearchConfig {
     @Bean
     ElasticsearchClient elasticsearchClient(
--- src/test/java/com/docbridge/docbridge/DocbridgeApplicationTests.java
+++ src/test/java/com/docbridge/docbridge/DocbridgeApplicationTests.java
@@ -1,11 +1,13 @@
 package com.docbridge.docbridge;
 
+import com.docbridge.docbridge.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@Import(TestConfig.class)
 class DocbridgeApplicationTests {
 
 	@Test
